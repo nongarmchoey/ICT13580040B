41ab604c55df5432cbb8b5dac09a44b693921a06
Theerapat <kingkong_armza@hotmail.com>
2017-12-19 23:11:01 +0700

    project
    midterm

---
+++ ICT13580040B/ICT13580040B/App.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace ICT13580040B
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            var np = new NavigationPage(new NavPage1());
+
+            var tp = new TabbedPage();
+            tp.Children.Add(new TabPage1());
+            tp.Children.Add(new TabPage2());
+            tp.Children.Add(new TabPage3());
+
+
+
+            var cp = new CarouselPage();
+            cp.Children.Add(new TabPage1());
+            cp.Children.Add(new TabPage2());
+            cp.Children.Add(new TabPage3());
+
+
+            var mp = new MasterDetailPage();
+            mp.Master = new MenuPage();
+            mp.Detail = new NavigationPage(new ICT13580040B.MainPage());
+
+            MainPage = mp;
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
